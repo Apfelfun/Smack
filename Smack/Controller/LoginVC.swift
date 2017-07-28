a37affc1896936063ce0630d38b54f1114a6d53d
Yousef Ouarghi <www_yousef@Yousefs-Mac-mini-3.local>
2017-07-28 16:26:23 +0200

    Video 4 beendet

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Yousef Ouarghi on 28.07.17.
+//  Copyright © 2017 Yousef Ouarghi. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Yousef Ouarghi on 28.07.17.
+//  Copyright © 2017 Yousef Ouarghi. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
+let TO_LOGIN = "toLogin"
+
